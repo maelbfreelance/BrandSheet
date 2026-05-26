f3458158a652c500591b846b86d33cb361b39173
maelbfreelance <mael.brianchon@gmail.com>
2026-05-26 19:38:11 +0200

    fix

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,11 +1,6 @@
 'use client'
 import React from 'react'
-import { createClient } from '@supabase/supabase-js'
-
-const supabase = createClient(
-  'https://aczakaoncltqrgxmlpxn.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFjemFrYW9uY2x0cXJneG1scHhuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk1ODUxNDUsImV4cCI6MjA5NTE2MTE0NX0.cKTkOlbxweCrvijLXl0kQKM4ALbaz4u1z-wUFclzC8s'
-)
+import { supabase } from '@/lib/supabase'
 
 export default function Login() {
   const handleGoogle = async () => {
